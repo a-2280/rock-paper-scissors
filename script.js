2eed87b81a03dbf314d4b3ad219279bf1d02237e
a-2280 <107672879+a-2280@users.noreply.github.com>
2024-03-15 21:30:23 -0600

    Remake the play five rounds function with a prompt

--- script.js
+++ script.js
@@ -1,7 +1,59 @@
+//Get a random computer choice
 function getComputerChoice() {
     const rps = ['Rock', 'Paper', 'Scissors'];
     const computerChoice = Math.floor(Math.random() * rps.length);
     return rps[computerChoice];
 };
 
-console.log(getComputerChoice());
+//Play a round of Rock Paper Scissors
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    if (playerSelection !== 'Rock' && playerSelection !== 'Paper' && playerSelection !== 'Scissors') {
+        return 'Not an option';
+    }
+    
+    if (playerSelection === computerSelection) {
+        if ( playerScore === computerScore) {
+            return `Tie.    Player: ${playerScore}  Computer: ${computerScore}`;
+        } else if (playerScore > computerScore){
+            return `You win!    Player: ${playerScore}  Computer: ${computerScore}`;
+        } else {
+            return `You lose.    Player: ${playerScore}  Computer: ${computerScore}`;
+        }
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === 'Scissors' && computerSelection === 'Paper' || playerSelection === 'Paper' && computerSelection === 'Rock') {
+        playerScore++;
+        if ( playerScore === computerScore) {
+            return `Tie.    Player: ${playerScore}  Computer: ${computerScore}`;
+        } else if (playerScore > computerScore){
+            return `You win!    Player: ${playerScore}  Computer: ${computerScore}`;
+        } else {
+            return `You lose.    Player: ${playerScore}  Computer: ${computerScore}`;
+        }
+    } else {
+        computerScore++;
+        if ( playerScore === computerScore) {
+            return `Tie.    Player: ${playerScore}  Computer: ${computerScore}`;
+        } else if (playerScore > computerScore){
+            return `You win!    Player: ${playerScore}  Computer: ${computerScore}`;
+        } else {
+            return `You lose.    Player: ${playerScore}  Computer: ${computerScore}`;
+        }
+    }
+};
+
+//Plays 5 round of Rock Paper Scissors
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const fiveTimes = playRound(prompt(), getComputerChoice());
+        console.log(fiveTimes);
+        if (i === 4) {
+            return fiveTimes;
+        }
+    }
+};
+
+console.log(playGame());
